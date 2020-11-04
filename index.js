377003f4799a00d85c14654a8b76cc5558c191fe
Ablax <ablax@ablax.me>
2020-11-04 21:35:21 +0200

    get token from evn + change delete to warn

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const client = new Client();
 const emojiRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
 const dcEmojiRegex = /<:.+?:\d+>/g;
 
+const token = process.env.TOKEN;
+
+if (!token) {
+    console.log('No token providen!')
+    return;
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setPresence({
@@ -27,19 +34,19 @@ client.on('ready', () => {
 
 const checkMessage = (msg) => {
     try {
-        if (!msg.guild || msg.guild.id !== '760562739298304095') {
-            return;
-        }
         const attachments = msg.attachments;
-        if(attachments)
-        if (attachments.array().length > 0) return;
+        if (attachments) {
+            if (attachments.array().length > 0) {
+                return;
+            }
+        }
 
         let replaced = msg.content.replace(dcEmojiRegex, '');
         replaced = replaced.replace(emojiRegex, '');
 
         let length = replaced.length;
         if (length === 0) {
-            deleteMsg(msg);
+            warn(msg);
         }
 
     } catch (e) {
@@ -47,13 +54,18 @@ const checkMessage = (msg) => {
     }
 }
 
-const deleteMsg = (msg) => {
-    msg.delete().then(deletedMsg => {
-        console.log(`Deleted message '${deletedMsg.content}' from ${deletedMsg.author.username}#${deletedMsg.author.discriminator}, for using emoji!`);
-    });
+const warn = (msg) => {
+    msg.channel.startTyping(5);
+    setTimeout(() => {
+        msg.channel.send(`<@${msg.author.id}> Моля те, стига си пращал емотикони на един ред! :poop:`)
+            .then(msg => {
+                msg.channel.stopTyping();
+            })
+    }, 1000);
+
 }
 
 client.on('message', msg => checkMessage(msg));
 client.on('messageUpdate', (oldMsg, newMsg) => checkMessage(newMsg));
 
-client.login('NzczNTMwNDQ5NjMyNjkwMjE3.X6KkXg.2lP1dKSUGYjRPJvbugNZI6jIs-U');
+client.login(token);
